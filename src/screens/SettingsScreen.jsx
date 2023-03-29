76e396810a8823beb89a6252bc61bf983694cfc0
Jonas Maches Gauiran <jonasmaches@gmail.com>
2023-03-29 14:31:17 +0000

    Upload New File

---
+++ src/screens/SettingsScreen.jsx
@@ -0,0 +1,82 @@
+import { React, useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+    Text,
+    View,
+    TextInput,
+} from 'react-native';
+
+//styles
+import { settingsScreen } from '../styles/Styles';
+
+export const SettingsScreen = (props) => {
+
+    const [text, setText] = useState('');
+
+    const getEmail = async () => {
+        try {
+            let email = await AsyncStorage.getItem('mail')
+            setText(email)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const changeEmail = async (value) => {
+        try {
+            await AsyncStorage.setItem('mail', value)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useFocusEffect(
+        useCallback(
+            () => {
+                getEmail()
+            },
+            [],
+        )
+    )
+
+    return (
+        <View style={settingsScreen.container}>
+            <View style={settingsScreen.topContainer}>
+                <Icon.Button
+                    name='arrow-left'
+                    size={60}
+                    backgroundColor='transparent'
+                    underlayColor='none'
+                    onPress={() => props.navigation.goBack()}
+                />
+            </View>
+            <View style={settingsScreen.bodyContainer}>
+                <View style={settingsScreen.formContainer}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Mandar Correo a:</Text>
+                    <TextInput
+                        style={{ height: 40, backgroundColor: '#191919', borderRadius: 5 }}
+                        placeholder="Introduzca la matrícula"
+                        onChangeText={newText => setText(newText)}
+                        defaultValue={text}
+                    />
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flex: 1 }} />
+                        <Icon.Button
+                            name='check'
+                            size={40}
+                            color='green'
+                            backgroundColor='transparent'
+                            underlayColor='none'
+                            style={{ alignSelf: 'flex-end', }}
+                            onPress={async () => { changeEmail(text) }}
+                        />
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default SettingsScreen
